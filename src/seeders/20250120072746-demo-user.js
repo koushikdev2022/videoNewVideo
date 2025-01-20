52bc37300d2eebc2dc2e294bf3b8872cf9a252b7
Raj Goswami <raj351dev@gmail.com>
2025-01-20 18:59:50 +0530

    d

--- src/seeders/20250120072746-demo-user.js
+++ src/seeders/20250120072746-demo-user.js
@@ -1,7 +1,6 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
-const bcrypt = require('bcrypt');
 module.exports = {
   async up (queryInterface, Sequelize) {
     /**
@@ -13,18 +12,6 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
-    const hashedPassword = await bcrypt.hash('12345678', 10);;
-    // console.log(hashedPassword)
-    await queryInterface.bulkInsert('users', [
-      {
-        first_name: "Admin",
-        username:"admin",
-        email:"superadmin@yopmail.com",
-        phone:"8820355454",
-        password:hashedPassword,
-        role:1
-      }
-    ], {});
   },
 
   async down (queryInterface, Sequelize) {
