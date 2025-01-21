f5c14d26e8ed333ad86063b0bd0118d3740334d6
koushikdev22 <koushik.journey.mondal@gmail.com>
2025-01-21 16:46:37 +0530

    new

--- src/config/db.js
+++ src/config/db.js
@@ -8,6 +8,9 @@ const DB_DATABASE = process.env.DB_DATABASE;
 const DB_CLIENT   = process.env.DB_CLIENT;
 const DB_HOST     = process.env.DB_HOST; 
 
+
+console.log(DB_USERNAME,DB_PASSWORD,DB_DATABASE,DB_CLIENT,DB_HOST)
+
 const sequelize = new Sequelize(DB_DATABASE,  DB_USERNAME, DB_PASSWORD , {
     host: DB_HOST,
     dialect: DB_CLIENT
