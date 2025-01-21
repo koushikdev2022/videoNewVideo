3e3a669b5d56534cf3a9fce9517890cb2c531bc1
koushikdev22 <koushik.journey.mondal@gmail.com>
2025-01-21 17:20:30 +0530

    new

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const { app,port } = require("./src/main/main")
 
 const db = require("./src/config/db")
+console.log(db)
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
