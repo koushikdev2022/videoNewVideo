17ac4c8ad25956c0add6d8e2e10353ad5cac5420
koushikdev22 <koushik.journey.mondal@gmail.com>
2025-01-21 17:46:44 +0530

    new

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const { app,port } = require("./src/main/main")
 
-const db = require("./src/config/db")
-console.log(db)
+const sequelize = require("./src/config/db")
+console.log(sequelize)
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
