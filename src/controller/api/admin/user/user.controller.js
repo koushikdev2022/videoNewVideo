5a898587dcbf4ea80e83e1f032e3671ba8ff1f3e
koushikdev22 <koushik.journey.mondal@gmail.com>
2025-01-28 18:30:19 +0530

    new

--- src/controller/api/admin/user/user.controller.js
+++ src/controller/api/admin/user/user.controller.js
@@ -1,4 +1,4 @@
-const { User } = require("../../../../models")
+const { User,Transaction,Wallet,Plan } = require("../../../../models")
 
 exports.list = async (req, res) => {
     try {
@@ -73,6 +73,127 @@ exports.status = async (req, res) => {
             })
         }
     } catch (err) {
+        console.log("Error in login authController: ", err);
+        const status = err?.status || 400;
+        const msg = err?.message || "Internal Server Error";
+        return res.status(status).json({
+            msg,
+            status: false,
+            status_code: status
+        })
+    }
+}
+
+exports.transaction = async(req,res)=>{
+    try{
+          const userId = req?.body?.id
+          const limit = req?.body?.limit || 10;
+          const page = req?.body?.page || 1;
+          const offset = (page-1) * limit; 
+          const query = {
+              include:[{
+                 model:Plan,
+                 as:"Plan",
+                 required:false
+              }],
+              order:[['created_at','desc']],
+              where:{},
+              limit:limit,
+              offset:offset
+          }
+          query.where.user_id=userId
+          const count = await Transaction.count({
+            where:query.where,
+            distinct: true
+          })
+          const findTransaction = await Transaction.findAll(query)
+          if (findTransaction) {
+            res.status(200).json({
+                messsage: "data found",
+                status: true,
+                status_code: 200,
+                data: findTransaction,
+                data_count: count,
+                page: page
+            })
+        } else {
+            res.status(200).json({
+                messsage: "no data found",
+                status: true,
+                status_code: 200,
+                data: findTransaction,
+                data_count: count,
+                page: page
+            })
+        }
+    }catch (err) {
+        console.log("Error in login authController: ", err);
+        const status = err?.status || 400;
+        const msg = err?.message || "Internal Server Error";
+        return res.status(status).json({
+            msg,
+            status: false,
+            status_code: status
+        })
+    }
+}
+
+exports.wallet = async(req,res)=>{
+    try{
+          const userId = req?.body?.id
+          const query = {
+              where:{},
+          }
+          query.where.user_id=userId
+          const findWallet = await Wallet.findAll(query)
+          if (findWallet) {
+            res.status(200).json({
+                messsage: "data found",
+                status: true,
+                status_code: 200,
+                data: findWallet,
+            })
+        } else {
+            res.status(200).json({
+                messsage: "no data found",
+                status: true,
+                status_code: 200,
+                data: findWallet,
+            })
+        }
+    }catch (err) {
+        console.log("Error in login authController: ", err);
+        const status = err?.status || 400;
+        const msg = err?.message || "Internal Server Error";
+        return res.status(status).json({
+            msg,
+            status: false,
+            status_code: status
+        })
+    }
+}
+
+exports.walletFreeze = async(req,res)=>{
+    try{
+        const walletId = req?.body?.id
+        const walletData = await Wallet.findByPk(walletId)
+        const updateWallet = await walletData.update({
+            account_frize:!walletData?.account_frize
+        })
+        if(updateWallet){
+            res.status(200).json({
+                messsage: "update successfully",
+                status: true,
+                status_code: 200,
+            })
+        }else{
+            res.status(400).json({
+                messsage: "update failed",
+                status: false,
+                status_code: 400,
+            })
+        }
+    }catch (err) {
         console.log("Error in login authController: ", err);
         const status = err?.status || 400;
         const msg = err?.message || "Internal Server Error";
--- src/route/admin/adminUserRoute.js
+++ src/route/admin/adminUserRoute.js
@@ -3,9 +3,13 @@ const adminUserRoute = express.Router();
 
 const userController = require("../../controller/api/admin/user/user.controller")
 const userStatusValidation = require("../../validations/admin/userStatusValidation")
-
+const walletFreezeValidation = require("../../validations/admin/walletFreezeValidation")
 
 adminUserRoute.post("/list",userController.list);
 adminUserRoute.post("/status",userStatusValidation,userController.status);
+adminUserRoute.post("/transaction",userStatusValidation,userController.transaction);
+adminUserRoute.post("/wallet",userStatusValidation,userController.wallet);
+adminUserRoute.post("/wallet-freeze",walletFreezeValidation,userController.walletFreeze);
+
 
 module.exports = adminUserRoute;
