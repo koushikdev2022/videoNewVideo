e6ac17f96037f90e68e135f429f3571733f8e731
koushikdev22 <koushik.journey.mondal@gmail.com>
2025-01-28 13:07:29 +0530

    new

--- src/controller/api/admin/auth/auth.controller.js
+++ src/controller/api/admin/auth/auth.controller.js
@@ -13,27 +13,36 @@ exports.login = async(req,res) =>{
                 role:1,
             }
         });
-        const passwordMatch = await checkPassword(password, user?.password);
-        if (!passwordMatch) {
+        if(user){
+            const passwordMatch = await checkPassword(password, user?.password);
+            if (!passwordMatch) {
+                return res.status(400).json({
+                    status: false,
+                    message: 'Invalid credential',
+                    status_code: 400,
+                });
+            }
+            const token = await generateAdminAccessToken(user);
+            const refresh = await adminRefreshAccessToken(user);
+            await User.update(
+                { refresh_token: refresh },
+                { where: { id: user.id } }
+            );
+            return res.status(200).json({
+                status: true,
+                message: 'User loggedin successfully',
+                status_code: 200,
+                user_token: token,
+                refresh_token: refresh
+            });
+        }else{
             return res.status(400).json({
                 status: false,
-                message: 'Invalid credential',
+                message: 'invalid username',
                 status_code: 400,
             });
         }
-        const token = await generateAdminAccessToken(user);
-        const refresh = await adminRefreshAccessToken(user);
-        await User.update(
-            { refresh_token: refresh },
-            { where: { id: user.id } }
-        );
-        return res.status(200).json({
-            status: true,
-            message: 'User loggedin successfully',
-            status_code: 200,
-            user_token: token,
-            refresh_token: refresh
-        });
+       
     } catch (err) {
         console.log("Error in login authController: ", err);
         const status = err?.status || 400;
