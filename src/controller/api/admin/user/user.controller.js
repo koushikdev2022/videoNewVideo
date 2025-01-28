b01af7f96568ff7b6baa68b60d003ab3fce43264
Suvendu <suvendu.iksen@gmail.com>
2025-01-28 14:11:08 +0530

    msg

--- src/controller/api/admin/user/user.controller.js
+++ src/controller/api/admin/user/user.controller.js
@@ -1,41 +1,41 @@
-const {User} = require("../../../../models")
+const { User } = require("../../../../models")
 
-exports.list = async(req,res)=>{
-       try{
-            const limit = req?.body?.limit || 10;
-            const page = req?.body?.page || 1;
-            const offset = (page-1)*limit;
-            const query = {
-                where:{},
-                order:[['created_at','desc']],
-                limit:limit,
-                offset:offset,
-            }
-            const totalUser = await User.count({
-                where:query.where,
-                distinct:true
+exports.list = async (req, res) => {
+    try {
+        const limit = req?.body?.limit || 10;
+        const page = req?.body?.page || 1;
+        const offset = (page - 1) * limit;
+        const query = {
+            where: {},
+            order: [['created_at', 'desc']],
+            limit: limit,
+            offset: offset,
+        }
+        const totalUser = await User.count({
+            where: query.where,
+            distinct: true
+        })
+        const user = await User.findAll(query)
+        if (user) {
+            res.status(200).json({
+                messsage: "data found",
+                status: true,
+                status_code: 200,
+                data: user,
+                data_count: totalUser,
+                page: page
             })
-            const user = await User.findAll(query)
-            if(user){
-                res.status(200).json({
-                    messsage:"data found",
-                    status:true,
-                    status_code:200,
-                    data:user,
-                    data_count:totalUser,
-                    page:page
-                })
-            }else{
-                res.status(200).json({
-                    messsage:"no data found",
-                    status:true,
-                    status_code:200,
-                    data:user,
-                    data_count:totalUser,
-                    page:page
-                })
-            }
-       }catch (err) {
+        } else {
+            res.status(200).json({
+                messsage: "no data found",
+                status: true,
+                status_code: 200,
+                data: user,
+                data_count: totalUser,
+                page: page
+            })
+        }
+    } catch (err) {
         console.log("Error in login authController: ", err);
         const status = err?.status || 400;
         const msg = err?.message || "Internal Server Error";
@@ -48,27 +48,31 @@ exports.list = async(req,res)=>{
 }
 
 
-exports.status = async(req,res)=>{
-    try{
+exports.status = async (req, res) => {
+    try {
         const userid = req?.body?.id
-        const userData = await  User?.findByPk(userid)
+        const userData = await User?.findByPk(userid)
+        console.log("UserData: ", userData);
+
         const updateData = await userData?.update({
-                 is_active:!updateData?.is_active
+            is_active: !updateData?.is_active
         })
-        if(updateData){
+        console.log("Update Data:", updateData);
+
+        if (updateData) {
             res.send(200).json({
-                messsage:"update successfully",
-                status:true,
-                status_code:200,
+                messsage: "update successfully",
+                status: true,
+                status_code: 200,
             })
-        }else{
+        } else {
             res.send(400).json({
-                messsage:"updation failed",
-                status:false,
-                status_code:400,
+                messsage: "updation failed",
+                status: false,
+                status_code: 400,
             })
         }
-    }catch (err) {
+    } catch (err) {
         console.log("Error in login authController: ", err);
         const status = err?.status || 400;
         const msg = err?.message || "Internal Server Error";
