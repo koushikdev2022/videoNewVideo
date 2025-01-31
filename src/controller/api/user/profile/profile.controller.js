9eef0b47e7666efb62e618998c1184eeb4d5bb04
koushikdev22 <koushik.journey.mondal@gmail.com>
2025-01-31 15:12:49 +0530

    new

--- src/controller/api/user/profile/profile.controller.js
+++ src/controller/api/user/profile/profile.controller.js
@@ -1,7 +1,44 @@
 
-
+const {User} = require("../../../../models")
 
 
 exports.update = async (req,res) =>{
-    
+    try{
+        const payload = req?.body
+        const userId = req?.id
+        const profileUpdate = await User.update({
+            full_name:payload?.full_name,
+            first_name:payload?.first_name,
+            last_name:payload?.last_name,
+            email :payload?.email ,
+            phone :payload?.phone ,
+            dob:payload?.dob
+        },{
+            where:{
+                id:userId
+            }
+        })
+        if(profileUpdate){
+            res.status(200).json({
+                message:"update successfully",
+                status:true,
+                status_code:200
+            })
+        }else{
+            res.status(400).json({
+                message:"update successfully",
+                status:false,
+                status_code:400
+            })
+        }
+    }catch (err) {
+        console.log("Error in login authController: ", err);
+        const status = err?.status || 400;
+        const msg = err?.message || "Internal Server Error";
+        return res.status(status).json({
+            msg,
+            status: false,
+            status_code: status
+        })
+    }
 }
