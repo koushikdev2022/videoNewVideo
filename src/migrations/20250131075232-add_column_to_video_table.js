7177a1f651d30e2e744ac37e378ee6c4cbfc4e98
koushikdev22 <koushik.journey.mondal@gmail.com>
2025-01-31 19:11:06 +0530

    new

--- src/migrations/20250131075232-add_column_to_video_table.js
+++ src/migrations/20250131075232-add_column_to_video_table.js
@@ -4,7 +4,7 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.addColumn('videos', 'thumbnail', {
-      type: Sequelize.STRING, 
+      type: Sequelize.TEXT('long'), 
       allowNull: true,        
       after: 'video_type',
     });
